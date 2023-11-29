0a03ca55e5ccce91abe8d8a38d66b61cf7295a0a
leopoldo.serpa <leopoldo.serpa@gmail.com>
2023-11-29 20:15:27 -0300

    pequeños cambios

--- src/Ensambles/ensamble_3_5.R
+++ src/Ensambles/ensamble_3_5.R
@@ -1,15 +1,7 @@
-#setwd('~/buckets/b1')
-
-setwd("C:/Users/leopo/Desktop/Maestria DATA MINING/DMEyF/KAGGLE 3RA COMPETENCIA")
+setwd('~/buckets/b1')
 require("data.table")
 
-# pengue <- fread("exp/KA8240_03_pengue_marchesini/prediccion.txt")
-# casalli <- fread("exp/final_casalli/prediccion.txt")
-# serpa <- fread('exp/KA8240_serpa/prediccion.txt')
-# flores <- fread('exp/KA5240_vanesa_flores/prediccion.txt')
-# lags <- fread('exp/exp_final/V4_2_prob.csv')[,list(numero_de_cliente,foto_mes,prob)]
-#ensamble_2 <- fread('exp/KA8240_ensamble_2_LRalto_neg_bagging_FILAS/prediccion.csv')
-#ensamble_5 <- fread('exp/KA8240_ensamble_5/prediccion.csv')
+
 #ensamble_1 <- fread("KA8240_ensamble_1/prediccion.csv")
 #ensamble_2 <- fread('KA8240_ensamble_2_LRalto_neg_bagging_FILAS/prediccion.csv', drop = c("numero_de_cliente","foto_mes"))
 ensamble_3 <- fread("exp/KA8240_ensamble_3/prediccion.csv")
