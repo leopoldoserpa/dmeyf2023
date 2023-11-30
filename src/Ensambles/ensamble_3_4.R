ce161ae57317daeb1e19427d17332594ab11adcd
leopoldo.serpa <leopoldo.serpa@gmail.com>
2023-11-29 21:36:00 -0300

    ensamble modelo 3 y 4

---
+++ src/Ensambles/ensamble_3_4.R
@@ -0,0 +1,38 @@
+setwd('~/buckets/b1')
+require("data.table")
+
+
+#ensamble_1 <- fread("KA8240_ensamble_1/prediccion.csv")
+#ensamble_2 <- fread('KA8240_ensamble_2_LRalto_neg_bagging_FILAS/prediccion.csv', drop = c("numero_de_cliente","foto_mes"))
+ensamble_3 <- fread("exp/KA8240_ensamble_3/prediccion.csv")
+ensamble_4 <- fread("exp/KA8240_ensamble_4/prediccion.csv", drop = c("numero_de_cliente","foto_mes"))
+#ensamble_5 <- fread('KA8240_ensamble_5/prediccion.csv', drop = c("numero_de_cliente","foto_mes"))
+
+# Concatenar los ensambles
+df <- cbind(ensamble_3, ensamble_4)
+
+# Calcular el promedio
+df[, prob_avg := rowMeans(df[, .SD, .SDcols = -(1:2)], na.rm = TRUE)]
+
+# Seleccionar las columnas necesarias
+columnas_necesarias <- c("numero_de_cliente", "prob_avg")
+
+# Mostrar el resultado
+df <- df[, ..columnas_necesarias]
+
+#Ordeno por probabilidad descendente
+setorder(df,-prob_avg)
+
+
+#Creo las predicciones para Kaggle
+dir.create("exp/ensamble_modelos")
+cortes <- seq(8000, 15000, by = 500)
+for (envios in cortes) {
+  df[, Predicted := 0L]
+  df[1:envios, Predicted := 1L]
+  
+  fwrite(df[, list(numero_de_cliente, Predicted)],
+         file = paste0("exp/ensamble_modelos/ensamble_3_4", "_", envios, ".csv"),
+         sep = ","
+  )
+}
